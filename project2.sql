fb945c5434b44ef722f7543475e09bfafd117d8e
Havu2910 <94585274+Havu2910@users.noreply.github.com>
2024-06-08 03:13:19 +0700

    Update project2.sql

--- project2.sql
+++ project2.sql
@@ -1,1 +1,10 @@
 --1. Số lượng đơn hàng và số lượng khách hàng mỗi tháng
+select 
+FORMAT_DATE("%Y-%m", created_at) AS Month_Year,
+SUM(DISTINCT user_id) AS total_user,
+SUM(DISTINCT order_id) AS total_order
+from bigquery-public-data.thelook_ecommerce.orders
+WHERE created_at between '2019-01-01' and '2022-04-30'
+GROUP BY created_at
+ORDER BY created_at
+--insight: 
